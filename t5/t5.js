491619a551d48987b9657f6285b387c3849c9bde
suvijohanna <suvi.mynttinen@metropolia.fi>
2025-08-22 13:28:40 +0300

    tehtävä 5 päivitetty

--- t5/t5.js
+++ t5/t5.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const integ = prompt('Syötä positiivinen kokonaisluku');
+const integ = +prompt('Syötä positiivinen kokonaisluku');
 
 let sum = 0;
 
-for (let i = 1; i <= integ ; i++) {
+for (let i = integ; i >= 1 ; i--) {
   sum += i;
 }
 
-document.querySelector('#target').innerHTML = sum;
+document.querySelector('#target').innerText = `Luonnollisten lukujen summa on ${sum}`;
